c73936095ae35a8caf4294ed0c5ad5f326cead8d
MatheusOtavio <MatheusOtavio@192.168.2.5>
2016-03-20 14:54:48 -0300

    2;

--- src/main/java/ConexãoComOBancoDeDados/ConexãoComOBancoDeDados.java
+++ src/main/java/ConexãoComOBancoDeDados/ConexãoComOBancoDeDados.java
@@ -8,21 +8,24 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.DBCursor;
 import com.mongodb.ServerAddress;
+
 import java.util.Arrays;
 
+import lombok.Getter;
+
 public class ConexãoComOBancoDeDados {
+	
 	public static void main( String args[] ){
 	 
 	 try{ 
-	 
-		 //Começando a conexão
-		 MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
- 
-		 //Base que desejo usar
-		 DB db = mongoClient.getDB( "TrabalhoBancoDeDados" );
-		 System.out.println("A conexão foi realizada com sucesso");
+		 MongoClient mongoClient = new MongoClient( "localhost" , 27017 );//Começando a conexão
+		 DB db = mongoClient.getDB( "TrabalhoBancoDeDados" ); //Base que desejo usar
+		 System.out.println("Conexão com MONGODB foi realizada com sucesso!");
 	 }catch(Exception e){
 		 System.err.println( e.getClass().getName() + ": " + e.getMessage() );
  }
  }
+
+	
+	
 }
