ac90b6e867ba5568b2c82220a774dc1170de3a1e
Matheus Ribak <matheusribak@hotmail.com>
2016-03-22 21:03:40 -0300

    Correções nas Classes

--- src/main/java/Model/Celular.java
+++ src/main/java/Model/Celular.java
@@ -13,7 +13,8 @@ public class Celular {
 
 	public final static String NOME_COLLECTION = Celular.class.getSimpleName()
 			.toLowerCase();
-	private String marca;
+	private String modelo; // Exemplos: Nokia xxx, iPhone5s, Sansung s7
+	private String empresa; //Nokia, Apple, entre outros. 
 	private String descricao;
 	private String camera;
 	private String memoria;
