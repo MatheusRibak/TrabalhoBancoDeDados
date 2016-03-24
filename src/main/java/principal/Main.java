ceaae93fb68a453915b605ce8a3796a008a664f5
MatheusOtavio <MatheusOtavio@Matheus-HP>
2016-03-24 00:32:16 -0300

    Commit com insert de pessoa e endereço juntos.

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -1,7 +1,11 @@
 package principal;
 
+import java.time.Instant;
+import java.util.Date;
+
 import DAO.GenericDAO;
 import Model.Endereco;
+import Model.Pessoa;
 
 public class Main {
 
@@ -14,7 +18,20 @@ public class Main {
 		endereco.setCidade("Ponte Serrada");
 		endereco.setCep("89683-000");
 		endereco.setUf("SC");
-		genericDao.inserir(endereco);
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Matheus Poletto");
+		pessoa.setRg("6.413.12");
+		pessoa.setCpf("987.123.122-90");
+		Instant instant = Instant.now();
+		pessoa.setDataNascimento(Date.from(instant));
+		pessoa.setEndereco(endereco);
+		pessoa.setTelefoneCelular("9955-5555");
+		pessoa.setEmail("mm.poletto@gmail.com");
+		
+		genericDao.inserir(pessoa);
+		
+		
 		
 	}
 	
