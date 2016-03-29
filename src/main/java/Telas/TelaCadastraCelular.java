b698dd82b2f5b32884ab5c75f35a674294f86f71
Matheus Poletto <motaviop@gmail.com>
2016-03-28 21:58:08 -0300

    Commit com tela de cadastro de celular pronta.

--- src/main/java/Telas/TelaCadastraCelular.java
+++ src/main/java/Telas/TelaCadastraCelular.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,12 +27,16 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 	private CriaButton cb = new CriaButton();
 	private FieldEmUpper fu = new FieldEmUpper();
 	private JLabel jlbCadDescricao, jlbCadImei, jlbCadModelo, jlbCadEmpresa, jlbCadCameraFrontal, jlbCadCameraTraseira,
-		jlbCadMemRam, jlbCadMemInt, jlbCadQtdChips, jlbCadValor, jlbCadTipoChip, jlbCadTela;
+		jlbCadMemRam, jlbCadMemInt, jlbCadQtdChips, jlbCadValor, jlbCadTipoChip, jlbCadTela, jlbCadSistema, jlbCadPeso, jlbCadCor,
+			jlbCadTv, jlbCadGarantia, jlbCadConectividade, jlbOpcOpcoes;
 	private JTextField jtfCadDescricao, jtfCadImei, jtfCadModelo, jtfCadEmpresa, jtfCadCameraFrontal, jtfCadCameraTraseira, 
-		jtfCadMemRam, jtfCadMemInt, jtfCadQtdChips, jtfCadValor, jtfCadTipoChip, jtfCadTela;
-	private JPanel jpnCadUnica, jpnCadAdj;
+		jtfCadMemRam, jtfCadMemInt, jtfCadQtdChips, jtfCadValor, jtfCadTipoChip, jtfCadTela, jtfCadSistema, jtfCadPeso, jtfCadCor, 
+			jtfCadGarantia, jtfCadConectividade;
+	private JPanel jpnCadUnica, jpnCadAdj, jpnOpcoes;
 	private JLabel jlbTituloFrame;
 	private Container tela;
+	private JComboBox<String> jcbPossuiTV;
+	private JButton jbtInfCadValor, jbtSalvar, jbtLimpar, jbtCancelar;
 	private static String titulo = "Cadastro de Celular";
 	
 	public TelaCadastraCelular() {
@@ -41,6 +47,17 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jlbTituloFrame = cl.criarTitulo(titulo, jlbTituloFrame, tela);
 		
 		criarCamposCadastrado();
+		
+		jlbOpcOpcoes = cl.criarParaPanelCenter("Opções", 360, 5, 80, 24, jlbOpcOpcoes, tela);
+		jbtLimpar = cb.criarBotao("Limpar", 600, 33, 100, 24, jbtLimpar, tela, this);
+		jbtSalvar = cb.criarBotao("Salvar", 300, 33, 100, 24, jbtSalvar, tela, this);
+		jbtCancelar = cb.criarBotao("Cancelar", 400, 33, 100, 24, jbtCancelar, tela, this);
+	
+		jpnOpcoes = cp.criarPanelSemTitulo(0, 400, 800, 90, jpnOpcoes, true, tela);
+		jpnOpcoes.add(jlbOpcOpcoes);
+		jpnOpcoes.add(jbtLimpar);
+		jpnOpcoes.add(jbtSalvar);
+		jpnOpcoes.add(jbtCancelar);
 		
 		tela.setBackground(Color.white);
 		setSize(800, 495);
@@ -57,7 +74,7 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jlbCadEmpresa = cl.criarLabelParaPanel("Empresa:", 100, 63, 160, 24, jlbCadEmpresa, tela);
 		
 		jtfCadDescricao = cf.criarTextField(100, 15, 600, 24, jtfCadDescricao, tela, this);
-		jtfCadImei = cf.criarTextField(100, 39, 80, 24, jtfCadImei, tela, this);
+		jtfCadImei = cf.criarTextField(100, 39, 167, 24, jtfCadImei, tela, this);
 		jtfCadModelo = cf.criarTextField(100, 63, 80, 24, jtfCadModelo, tela, this);
 		jtfCadEmpresa = cf.criarTextField(265, 63, 435, 24, jtfCadEmpresa, tela, this);
 			
@@ -78,6 +95,13 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jlbCadQtdChips = cl.criarLabelParaPanel("Quantidade de chips:", 5, 63, 160, 24, jlbCadQtdChips, tela);
 		jlbCadTipoChip = cl.criarLabelParaPanel("Tipo de chip:", 270, 63, 130, 24, jlbCadTipoChip, tela);
 		jlbCadTela = cl.criarLabelParaPanel("Tamanho da tela:", 15, 87, 150, 24, jlbCadTela, tela);
+		jlbCadSistema = cl.criarLabelParaPanel("Sist. Operacional:", 15, 111, 150, 24, jlbCadSistema, tela);
+		jlbCadPeso = cl.criarLabelParaPanel("Peso do aparelho:", 15, 135, 150, 24, jlbCadPeso, tela);
+		jlbCadCor = cl.criarLabelParaPanel("Cor:", 200, 135, 100, 24, jlbCadCor, tela);
+		jlbCadTv = cl.criarLabelParaPanel("Possui TV:", 336, 135, 150, 24, jlbCadTv, tela);
+		jlbCadConectividade = cl.criarLabelParaPanel("Conectividade:", 15, 159, 150, 24, jlbCadConectividade, tela);
+		jlbCadGarantia = cl.criarLabelParaPanel("Garantia:", 15, 183, 150, 24, jlbCadGarantia, tela);
+		jlbCadValor = cl.criarLabelParaPanel("Valor(R$):", 15, 207, 150, 24, jlbCadValor, tela);
 		
 		jtfCadCameraFrontal = cf.criarTextField(175, 15, 80, 24, jtfCadCameraFrontal, tela, this);
 		jtfCadCameraTraseira = cf.criarTextField(405, 15, 80, 24, jtfCadCameraTraseira, tela, this);
@@ -86,8 +110,23 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jtfCadQtdChips = cf.criarTextField(175, 63, 80, 24, jtfCadQtdChips, tela, this);
 		jtfCadTipoChip = cf.criarTextField(405, 64, 295, 24, jtfCadTipoChip, tela, this);
 		jtfCadTela = cf.criarTextField(175, 87, 80, 24, jtfCadTela, tela, this);
+		jtfCadSistema = cf.criarTextField(175, 111, 230, 24, jtfCadSistema, tela, this);
+		jtfCadPeso = cf.criarTextField(175, 135, 80, 24, jtfCadPeso, tela, this);
+		jtfCadCor = cf.criarTextField(305, 135, 100, 24, jtfCadCor, tela, this);
+		jtfCadConectividade = cf.criarTextField(175, 159, 530, 24, jtfCadConectividade, tela, this);
+		jtfCadGarantia = cf.criarTextField(175, 183, 530, 24, jtfCadGarantia, tela, this);		
+		jtfCadValor = cf.criarTextField(175, 207, 80, 24, jtfCadValor, tela, this);		
 		
-		jpnCadAdj = cp.criarPanelSemTitulo(0, 170, 800, 150, jpnCadAdj, true, tela);
+		jcbPossuiTV = new JComboBox<>();
+		jcbPossuiTV.setBounds(490, 135, 80, 24);
+		jcbPossuiTV.addItem("NÃO");
+		jcbPossuiTV.addItem("SIM");
+		jcbPossuiTV.setSelectedIndex(0);
+		
+		String MENSAGEM_VALOR = "Use apenas pontos no valor. Exemplo: 1.500 para mil e quinhentos reais.";
+		jbtInfCadValor = cb.criarBotaoAjuda(MENSAGEM_VALOR, "?", 255, 207, 49, 24, jbtInfCadValor, tela, this);	
+		
+		jpnCadAdj = cp.criarPanelSemTitulo(0, 170, 800,	237, jpnCadAdj, true, tela);
 		jpnCadAdj.add(jlbCadCameraFrontal);	
 		jpnCadAdj.add(jlbCadCameraTraseira);
 		jpnCadAdj.add(jlbCadMemInt);	
@@ -95,6 +134,13 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jpnCadAdj.add(jlbCadQtdChips);
 		jpnCadAdj.add(jlbCadTipoChip);
 		jpnCadAdj.add(jlbCadTela);
+		jpnCadAdj.add(jlbCadSistema);
+		jpnCadAdj.add(jlbCadPeso);
+		jpnCadAdj.add(jlbCadCor);
+		jpnCadAdj.add(jlbCadTv);
+		jpnCadAdj.add(jlbCadConectividade);
+		jpnCadAdj.add(jlbCadGarantia);
+		jpnCadAdj.add(jlbCadValor);
 		jpnCadAdj.add(jtfCadCameraFrontal);	
 		jpnCadAdj.add(jtfCadCameraTraseira);	
 		jpnCadAdj.add(jtfCadMemInt);	
@@ -102,7 +148,14 @@ public class TelaCadastraCelular extends JFrame implements ActionListener, KeyLi
 		jpnCadAdj.add(jtfCadQtdChips);
 		jpnCadAdj.add(jtfCadTipoChip);
 		jpnCadAdj.add(jtfCadTela);
-		
+		jpnCadAdj.add(jtfCadSistema);
+		jpnCadAdj.add(jtfCadPeso);
+		jpnCadAdj.add(jtfCadCor);
+		jpnCadAdj.add(jtfCadConectividade);
+		jpnCadAdj.add(jtfCadGarantia);
+		jpnCadAdj.add(jtfCadValor);
+		jpnCadAdj.add(jcbPossuiTV);
+		jpnCadAdj.add(jbtInfCadValor);
 	}
 
 	public static void main(String[] args) {
