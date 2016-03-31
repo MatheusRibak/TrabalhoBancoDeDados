4514ace1b664a6fbec83438517dd1292012985f2
Matheus Poletto <motaviop@gmail.com>
2016-03-31 14:36:02 -0300

    Commit com alteração nas mensagens de retorno e telefones.

--- src/main/java/utilitarios/EscolheMensagem.java
+++ src/main/java/utilitarios/EscolheMensagem.java
@@ -29,7 +29,7 @@ public class EscolheMensagem {
 			menSucesso.sucessoAlterarCliente();
 			break;	
 		case "alterar_vendedor":
-			menSucesso.sucessoAlterarCliente();
+			menSucesso.sucessoAlterarVendedor();
 			break;	
 		case "alterar_celular":
 			menSucesso.sucessoAlterarCelular();
