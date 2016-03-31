1501881a79e17e7f53450403b3ab723726fa01a6
Matheus Poletto <motaviop@gmail.com>
2016-03-31 16:09:34 -0300

    Commit com tela procurar venda pronta

--- src/main/java/Telas/TelaProcurarVenda.java
+++ src/main/java/Telas/TelaProcurarVenda.java
@@ -13,8 +13,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
 import metodos.ListarCelular;
@@ -65,52 +67,63 @@ public class TelaProcurarVenda extends JFrame implements ActionListener, KeyList
 	private void criarOpcoesResultado() {
 		jlbOpcOpcoes = cl.criarParaPanelCenter("Opções", 360, 5, 80, 24, jlbOpcOpcoes, tela);
 		jbtAlterar = cb.criarBotao("Alterar", 300, 33, 100, 24, jbtAlterar, tela, this);
-		//jbtExcluir = cb.criarBotao("Excluir", 400, 33, 100, 24, jbtExcluir, tela, this);
+		jbtExcluir = cb.criarBotao("Excluir", 400, 33, 100, 24, jbtExcluir, tela, this);
 		
 		jpnOpcoes = cp.criarPanelSemTitulo(0, 470, 800, 90, jpnOpcoes, true, tela);
 		jpnOpcoes.add(jlbOpcOpcoes);
 		jpnOpcoes.add(jbtAlterar);
-		//jpnOpcoes.add(jbtExcluir);
+		jpnOpcoes.add(jbtExcluir);
 		
 	}
 
 	private void criarTabela() {
 		ArrayList<String> colunas = new ArrayList<String>();
-		//colunas.add("ID");
-		colunas.add("DATA");
+		colunas.add("ID");
+		colunas.add("CLIENTE");
 		colunas.add("VENDEDOR");
 		colunas.add("VALOR(R$)");
 		
-		dtmCelulares = ct.criarTableImoveis(jtbCelulares, tela, dtmCelulares, colunas, 0, 170, 800, 300);		
+		jtbCelulares = new JTable();
+		tela.add(jtbCelulares);
+		dtmCelulares = new DefaultTableModel();
+		for (String colNome : colunas) {
+			dtmCelulares.addColumn(colNome);
+		}
+		jtbCelulares.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jtbCelulares.setModel(dtmCelulares);
+		JScrollPane jsp = new JScrollPane(jtbCelulares);
+		jsp.setBounds(0, 170, 800, 300);
+		jsp.setVisible(true);
+		tela.add(jsp);	
 	}
 
 	private void criarCamposBusca() {
 		jlbProCliente = cl.criarLabelParaPanel("Cliente:", 5, 15, 90, 24, jlbProCliente, tela);
-		jlbProData = cl.criarLabelParaPanel("Data:", 15, 39, 80, 24, jlbProData, tela);
-		jlbProVendedor = cl.criarLabelParaPanel("Vendedor:", 345, 39, 100, 24, jlbProVendedor, tela);
-		jlbProCodNF = cl.criarLabelParaPanel("Cód. NF:", 15, 63, 80, 24, jlbProCodNF, tela);
+		//jlbProData = cl.criarLabelParaPanel("Data:", 15, 39, 80, 24, jlbProData, tela);
+		jlbProVendedor = cl.criarLabelParaPanel("Vendedor:", 5, 39, 90, 24, jlbProVendedor, tela);
+		//jlbProCodNF = cl.criarLabelParaPanel("Cód. NF:", 15, 63, 80, 24, jlbProCodNF, tela);
 		
 		jtfProCliente = cf.criarTextField(100, 15, 600, 24, jtfProCliente, tela, this);
-		jtfProData = cf.criarMaskFieldDate(100, 39, 80, 24, jtfProData, tela);
-		jtfProVendedor = cf.criarTextField(450, 39, 250, 24, jtfProVendedor, tela, this);
-		jtfProCodNF = cf.criarTextField(100, 63, 80, 24, jtfProCodNF, tela, this);
+		//jtfProData = cf.criarMaskFieldDate(100, 39, 80, 24, jtfProData, tela);
+		jtfProVendedor = cf.criarTextField(100, 39, 600, 24, jtfProVendedor, tela, this);
+		//jtfProCodNF = cf.criarTextField(100, 63, 80, 24, jtfProCodNF, tela, this);
 		
 		jbtNovo = cb.criarBotao("Novo", 500, 63, 100, 24, jbtNovo, tela, this);
 		jbtProcurar = cb.criarBotao("Procurar", 600, 63, 100, 24, jbtProcurar, tela, this);
 		
-		String MENSAGEM_DATA = "Use o formato Mês/Dia/Ano ao inserir datas. Exemplo: 12/29/97";
-		jbtInfData = cb.criarBotaoAjuda(MENSAGEM_DATA, "?", 180, 39, 49, 24, jbtInfData, tela, this);		
+		//String MENSAGEM_DATA = "Use o formato Mês/Dia/Ano ao inserir datas. Exemplo: 12/29/97";
+		//jbtInfData = cb.criarBotaoAjuda(MENSAGEM_DATA, "?", 180, 39, 49, 24, jbtInfData, tela, this);		
 		
 		jpnCamposBusca = cp.criarPanelSemTitulo(0, 70, 800, 100, jpnCamposBusca, true, tela);
 		jpnCamposBusca.add(jlbProCliente);
 		jpnCamposBusca.add(jtfProCliente);
-		jpnCamposBusca.add(jlbProData);
-		jpnCamposBusca.add(jtfProData);
-		jpnCamposBusca.add(jbtInfData);
+		//jpnCamposBusca.add(jlbProData);
+		//jpnCamposBusca.add(jtfProData);
+		//jpnCamposBusca.add(jbtInfData);
 		jpnCamposBusca.add(jlbProVendedor);
 		jpnCamposBusca.add(jtfProVendedor);
-		jpnCamposBusca.add(jlbProCodNF);
-		jpnCamposBusca.add(jtfProCodNF);
+		//jpnCamposBusca.add(jlbProCodNF);
+		//jpnCamposBusca.add(jtfProCodNF);
 		jpnCamposBusca.add(jbtNovo);
 		jpnCamposBusca.add(jbtProcurar);
 		
